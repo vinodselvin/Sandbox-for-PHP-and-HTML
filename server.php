4e178c7fbfb05197b352c479a66cfe951b972776
Markus Deibel <github2018@markus-deibel.de>
2018-10-04 20:49:03 +0200

    Avoid unnecessary setting of $txt
    If POST['codes'] is empty skip the setting part.

--- server.php
+++ server.php
@@ -1,18 +1,14 @@
 <?php
 include "./config.php";
 
-/*if(empty($_COOKIE['continue'])){
-    setcookie('continue', uniqid().rand(10,5000), time() + (86400 * 300), "/"); // 86400 = 1 day
-}*/
 error_reporting(0);
 $myfile = fopen($_COOKIE['continue'].".php", "w") or die("Unable to open file!");
 
-$txt = "<?php error_reporting(E_ALL); ini_set('display_errors', 1);?>\n";
+$txt = "";
 
-$txt .= $_POST['codes'];
-
-if(empty($_POST['codes'])){
-    $txt = "";
+if(!empty($_POST['codes'])){
+    $txt = "<?php error_reporting(E_ALL); ini_set('display_errors', 1);?>\n";
+    $txt .= $_POST['codes'];
 }
 
 fwrite($myfile, $txt);
